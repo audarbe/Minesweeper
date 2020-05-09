cb54689e77c125b78fc12b34479b9c6d76b955df
Alexei Udarbe <alexeiudarbe@AU-MacBook-Bro.local>
2020-05-08 18:35:03 -0700

    HTML for testing,  states,, prelim functions

--- js/main.js
+++ js/main.js
@@ -1,101 +1,99 @@
-//***************************************
-// pseudocode - Last Revision - 050820
-//***************************************
+/*----- constants -----*/
+const difficultyLookup = {
+  outbreak: {
+    axisX: 10,
+    axisY: 10,
+    coronas: 10,
+    winAmount: this.axisX * this.axisY - this.coronas,
+    color: "#9ACD32", //yellowgreen
+  },
+  epidemic: {
+    axisX: 15,
+    axisY: 13,
+    coronas: 40,
+    winAmount: this.axisX * this.axisY - this.coronas,
+    color: "#FF8C00", //darkorange
+  },
+  pandemic: {
+    axisX: 30,
+    axisY: 16,
+    coronas: 99,
+    winAmount: this.axisX * this.axisY - this.coronas,
+    color: "#FF4500", //orangered
+  },
+};
 
+const squareEl = {
+  occupied: {
+    color: "red",
+    image: "", // use background instead
+  },
+  uncover: {
+    color: "lightgray",
+    image: "",
+  },
+  available: {
+    color: "green",
+    image: "",
+  },
+  flagged: {
+    color: "yellow",
+    image: "",
+  },
+};
 
-// Determine the constants for the game
-    // Declare a variable with an object of difficulty levels and any objects we’ll need
-        // Squares on x-axis
-        // Squares on y-axis
-        // Background color reference
-        // Number of mines placed
-        // Score needed to win
-    // Declare a object with the states of our squares
-        // Occupied by mines
-            // color/Image reference
-            // We will use the index of the a square based on how our difficulty to occupy a square with a mine.
-            // Place the indexes into an array and assign to variable of mines.
-        // Uncovered State
-            // Color reference
-        // Covered State
-            // Color reference
-    // Create an object for the different states of our emoji guy
-        // Normal
-        // Active
-        // Lose
-        // Win
-    // Create an object for audio clips
-        // Uncover square
-            // Click
-            // Adjacent squares
-        // Game start
-        // Game reset
-        // Lose
-        // win
-    // Instantiate our winner accumulator variable to zero.
-    // Intantate our game board
-    // Instantiate difficulty
+const emoji = {
+  inPlay: "img/ejoji_inPlay.png",
+  click: "img/emoji_active.png",
+  lose: "img/ejoji_lose.png",
+  win: "img/emoji_win.png",
+};
 
-// Create any cached element references
-    // Emoji element for our “messaging”
-    // Body for background image changing
+const audioEl = {
+  flagged: "",
+  uncover: "",
+  start: "",
+  lose: "",
+  win: "",
+};
 
-// Establish any event listeners
-    // Create an event listener for our squares
-        // Will be used for our square actions and timer
-        // Also will invoke our handleClick function
-    // Create an event listener for our difficulty selector
-    // Create an event listener for our flag
-        // button in nav
-        // Right click on square
-    // Create an event listener for our reset button
-    // Create event listener for “cheat code”
-        // Sequence of keypresses
+/*----- app's state (variables) -----*/
+let difficulty = "epidemic";
+let currentScore;
+let board = [];
 
-// Create our Initialization function
-    // Create our board
-        // Reference our difficulty object for our game board information
-        // Append elements to the DOM to create board
-    // Set difficulty to spicy (default)
-    // Reset our timer and and a click event to start
+/*----- cached element references -----*/
+const diffcultySelector = document.querySelector("#difficulty-selector");
+const remainingCoronas = document.querySelector("#remaining-coronas");
+const emojiBox = document.querySelector("#emoji");
+const timer = document.querySelector("#timer");
+const flagToggle = document.querySelector("#flag");
+const resetButton = document.querySelector("#reset");
+const squareEls = Array.from(document.querySelectorAll("#coronaField > div"));
 
-// Create our handle click function
-    // If click on square
-        //invoke win-logic function
-    // If current index is not occupied by a mine, 
-        //invoke uncoverSquares functions 
-    // Clicks on flag icon or right clicks on a square element, invoke our flag function
-    // When game starts, change the emoji guy to our ‘normal’ state to render
-    // When user clicks on an square, send active state to render function
+/*----- event listeners -----*/
+diffcultySelector.addEventListener("change", changeDifficulty);
+resetButton.addEventListener("click", init);
 
-// Create uncoversquares function
-    // Check to see if adjacent squares are empty (check in the following pattern:
-            // Top, top-right, right, bottom-right, bottom, bottom-left, left, top-left
-                // While adjacent squares are empty
-                    // Check next set of adjacent squares
-                // If the adjacent square is occupied by a mine
-                    // Place the number of elements that have a mine in the uncovered element
-    // Add number of uncovered squares to our winner accumulator value.
+/*----- functions -----*/
 
-// Create our flagging function
-    // If the user clicks on our flag toggle
-        // Any clicks on a square element should place a image in the div of flag instead of checking for mines,
-        // When user clicks on a square that includes an image of the flag, remove the flag.
+function init() {
+  console.log("init");
+  render();
+}
 
-// Create our win-logic function
-    // If the current index matches anything in our occupied mines array.
-        // If winner is === -1, send losing parameter to render function.
-    // If winner is === to minimum based on difficulty,
-        //send win parameter to our render function.
+function createBoard() {}
 
-// Create our Render function
-    // Audio
-        // When user click on uncovered square
-        // Uncovering adjacent squares
-        // Game start
-        // Game end
-    // Display
-        // While game is playing, show normal emoji guy
-        // If argument is win, change emoji to sunglasses guy
-        // If argument is active, change emoji to shocked guy
-        // If argument is lose, change emoji to x-eyes guy
+function changeDifficulty() {
+  difficulty = diffcultySelector.value;
+  render();
+}
+
+function render() {
+  document.querySelector("body").style.backgroundColor =
+    difficultyLookup[difficulty].color;
+  document.querySelector("#remaining-coronas").innerText =
+    difficultyLookup[difficulty].coronas;
+}
+
+init();
---
+++ js/pseudoCode.js
@@ -0,0 +1,101 @@
+//***************************************
+// pseudocode - Last Revision - 050820
+//***************************************
+
+
+// Determine the constants for the game
+    ---> // Declare a variable with an object of difficulty levels and any objects we’ll need
+        ---> // Squares on x-axis
+        ---> // Squares on y-axis
+        ---> // Background color reference
+        ---> // Number of mines placed
+        ---> // Score needed to win
+    // Declare a object with the states of our squares
+        // Occupied by mines
+        ---> // color/Image reference
+            // We will use the index of the a square based on how our difficulty to occupy a square with a mine.
+            // Place the indexes into an array and assign to variable of mines.
+        ---> //Uncovered State
+        ---> // Color reference
+    ---> // Covered State
+        ---> /// Color reference
+    ---> // Create an object for the different states of our emoji guy
+        ---> // Normal
+        ---> // Active
+        ---> // Lose
+        ---> // Win
+    ---> //  Create an object for audio clips
+        ---> //  Uncover square
+            ---> //  Click
+            ---> //  Adjacent squares
+        ---> //  Game start
+        ---> //  Game reset
+        ---> //  Lose
+        ---> //  win
+    ---> // Instantiate our winner accumulator variable to zero.
+    // Intantate our game board
+    ---> //Instantiate difficulty
+
+// Create any cached element references
+    // Emoji element for our “messaging”
+    // Body for background image changing
+
+// Establish any event listeners
+    // Create an event listener for our squares
+        // Will be used for our square actions and timer
+        // Also will invoke our handleClick function
+   ---> // Create an event listener for our difficulty selector
+    // Create an event listener for our flag
+        // button in nav
+        // Right click on square
+---> // Create an event listener for our reset button
+    // Create event listener for “cheat code”
+        // Sequence of keypresses
+
+// Create our Initialization function
+    // Create our board
+        // Reference our difficulty object for our game board information
+        // Append elements to the DOM to create board
+---> // Set difficulty to spicy (default)
+     // Reset our timer and and a click event to start
+
+// Create our handle click function
+    // If click on square
+        //invoke win-logic function
+    // If current index is not occupied by a mine, 
+        //invoke uncoverSquares functions 
+    // Clicks on flag icon or right clicks on a square element, invoke our flag function
+    // When game starts, change the emoji guy to our ‘normal’ state to render
+    // When user clicks on an square, send active state to render function
+
+// Create uncoversquares function
+    // Check to see if adjacent squares are empty (check in the following pattern:
+            // Top, top-right, right, bottom-right, bottom, bottom-left, left, top-left
+                // While adjacent squares are empty
+                    // Check next set of adjacent squares
+                // If the adjacent square is occupied by a mine
+                    // Place the number of elements that have a mine in the uncovered element
+    // Add number of uncovered squares to our winner accumulator value.
+
+// Create our flagging function
+    // If the user clicks on our flag toggle
+        // Any clicks on a square element should place a image in the div of flag instead of checking for mines,
+        // When user clicks on a square that includes an image of the flag, remove the flag.
+
+// Create our win-logic function
+    // If the current index matches anything in our occupied mines array.
+        // If winner is === -1, send losing parameter to render function.
+    // If winner is === to minimum based on difficulty,
+        //send win parameter to our render function.
+
+// Create our Render function
+    // Audio
+        // When user click on uncovered square
+        // Uncovering adjacent squares
+        // Game start
+        // Game end
+    // Display
+        // While game is playing, show normal emoji guy
+        // If argument is win, change emoji to sunglasses guy
+        // If argument is active, change emoji to shocked guy
+        // If argument is lose, change emoji to x-eyes guy
